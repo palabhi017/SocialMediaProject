427d612c0a8fd6f8481f882f559794b2df68382e
Macbook pro <macbookpro@Piyushs-MacBook-Pro.local>
2025-08-25 21:59:58 +0530

    Comments Part Complete

--- src/api/socket.js
+++ src/api/socket.js
@@ -1,6 +1,6 @@
 import { io } from "socket.io-client";
 
-const SOCKET_URL = "http://localhost:3000";
+const SOCKET_URL = "http://localhost:5000";
 
 // single instance of socket
 export const socket = io(SOCKET_URL, {
